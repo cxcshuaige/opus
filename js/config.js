1e799945c19a3f97bc87f8a4971db117f82d8ac4
cxc <cxcshuaige@gmail.com>
2015-01-07 12:35:37 +0800

    f

--- js/config.js
+++ js/config.js
@@ -1,7 +1,10 @@
 seajs.config({
   // Set aliases for common libraries
 	alias: {
-		'$': 'jquery/jquery/1.10.1/jquery.js'
+		'$': 'jquery/jquery/1.10.1/jquery.js',
+		'TweenLite':'greensock/minified/TweenLite.min.js',
+		'CSSPlugin':'greensock/minified/plugins/CSSPlugin.min.js',
+		'ScrollToPlugin':'greensock/minified/plugins/ScrollToPlugin.min.js'
 	}
 
 });
